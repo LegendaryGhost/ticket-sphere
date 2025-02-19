3ee9064e83bb5a15a4686688dfc71e6664dc3078
Tiarintsoa <rihantiana000@gmail.com>
2025-02-19 11:46:19 +0300

    Admin logout

--- src/main/java/com/tiarintsoa/ticketsphere/controller/backOffice/AdminAuthController.java
+++ src/main/java/com/tiarintsoa/ticketsphere/controller/backOffice/AdminAuthController.java
@@ -1,10 +1,8 @@
 package com.tiarintsoa.ticketsphere.controller.backOffice;
 
-import com.tiarintsoa.annotation.Controller;
-import com.tiarintsoa.annotation.Post;
-import com.tiarintsoa.annotation.RequestParameter;
-import com.tiarintsoa.annotation.UrlMapping;
+import com.tiarintsoa.annotation.*;
 import com.tiarintsoa.controller.ModelView;
+import com.tiarintsoa.session.WinterSession;
 import com.tiarintsoa.ticketsphere.dao.AdminDAO;
 import com.tiarintsoa.ticketsphere.dto.LoginCredentials;
 import com.tiarintsoa.ticketsphere.model.Admin;
@@ -12,6 +10,8 @@ import com.tiarintsoa.ticketsphere.model.Admin;
 @Controller
 @UrlMapping("/admin-auth")
 public class AdminAuthController {
+
+    private WinterSession session;
 
     @UrlMapping("/login")
     public ModelView login() {
@@ -24,7 +24,20 @@ public class AdminAuthController {
         Admin admin = AdminDAO.findByEmailAndPassword(credentials);
         if (admin == null) {
             return new ModelView("back-office/login.jsp");
+        } else {
+            session.add("authenticated", true);
+            session.add("role", "admin");
+            session.add("adminId", admin.getId());
+            return new ModelView("back-office/home.jsp");
         }
+    }
+
+    @Get
+    @UrlMapping("/logout")
+    public ModelView logout() {
+        session.delete("authenticated");
+        session.delete("role");
+        session.delete("adminId");
         return new ModelView("back-office/home.jsp");
     }
 
